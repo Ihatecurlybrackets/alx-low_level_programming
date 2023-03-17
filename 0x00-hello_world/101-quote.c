6956819dcaf0287dfe41cd119bb46b67249de9e2
Hafssa Tartouchi <hafssatartouchi17@gmail.com>
2023-03-17 05:41:40 -0700

    hello3

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 1 when successful
+ */
+
+int main(void)
+{
+  fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+  return (1);
+}
