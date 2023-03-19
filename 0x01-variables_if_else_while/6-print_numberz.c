2a8510088edaabaf40b040285501d93f546c0897
Hafssa Tartouchi <hafssatartouchi17@gmail.com>
2023-03-19 14:27:30 -0700

    hello0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: returns 0 at success
+ */
+
+int main(void)
+{
+int n;
+for(n = 0; n <= 9; n++)
+{
+	putchar('0' + n);
+}
+return (0);
+}
