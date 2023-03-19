18d420f7bf24f20956aa8247162df3c259f78daa
Hafssa Tartouchi <hafssatartouchi17@gmail.com>
2023-03-19 14:04:30 -0700

    hello0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	int c;
+int c;
 
-	for (c = 0; c == 9; c++)
-	{
-		printf("%d",c);
-	}
-	printf("\n");
-	return (0);
+for (c = 0; c <= 9; c++)
+{
+	printf("%d", c);
+}
+printf("\n");
+return (0);
 }
